f7591a5d326890add89a0d04e09acb2ee68d8e63
Reza Kay <reza.abadi@dekatku.com>
2016-06-10 10:05:43 +0800

    bash is corrected for better lamp installation

--- scripts/ubuntu-lamp.sh
+++ scripts/ubuntu-lamp.sh
@@ -4,6 +4,8 @@ sudo apt-get update
 
 sudo apt-get -y install apache2
 
+ifconfig eth0 | grep inet | awk '{ print $2 }'
+
 sudo /etc/init.d/apache2 stop
 
 
@@ -11,26 +13,30 @@ rm -rf /var/www/html
 
 ln -s /vagrant /var/www/html
 
+sudo /etc/init.d/apache2 start
+
 #making login credentials or mysql
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
 
-sudo apt-get -y install mysql-server
+sudo apt-get -y install mysql-server libapache2-mod-auth-mysql php5-mysql  
+
 
 sudo mysql -u root -proot  -e "SHOW DATABASES;"
 
 
-sudo apt-get -y --ignore-missing install libapache2-mod-php5 
-
-sudo a2enmod php5
+sudo apt-get -y --ignore-missing install php5 libapache2-mod-php5 php5-mcrypt php5-cli php5-common
 
 
 # Download starter content
 
 cd /vagrant
 
-sudo -i -u vagrant wget -q https://raw.githubusercontent.com/rezakay/vagrant/master/files/index.html
+sudo -i -u vagrant wget -q https://raw.githubusercontent.com/rezakay/vagrant/master/files/index.html -P /vagrant
 sudo -i -u vagrant wget -q https://raw.githubusercontent.com/rezakay/vagrant/master/files/info.php
+-P /vagrant
+
+sudo /etc/init.d/apache2 restart
 
 php -r 'echo "\n\nYour PHP installation is working fine.\n\n\n";'
 
